39aa0bce38cae31ca99bd99803bb06179835267c
George <g9c9@outlook.com>
2025-03-03 21:08:05 -0800

    Print request details and use request parameter to input name

--- helloWorld.ts
+++ helloWorld.ts
@@ -4,8 +4,28 @@ function greet(name: string): string {
 
 console.log(greet("world"));
 
-const handler = (_req: Request) => {
-    return new Response(greet("Test"));
+
+// https://docs.deno.com/runtime/fundamentals/http_server/
+
+const handler = async (req: Request) => {
+    console.log(`Method: ${req.method}`);
+
+    const url = new URL(req.url);
+    console.log(`Path: ${url.pathname}`);
+    console.log(`Query parameters: ${url.searchParams}`);
+
+    console.log("Headers:");
+    req.headers.forEach((val, key, _parent) => {
+        console.log(`${key} : ${val}`);
+    })
+
+    if (req.body) {
+        // Await here ensures the body is fully read before logging
+        const body = await req.text();  
+        console.log(`Body: ${body}`);
+    }
+
+    return new Response(greet(url.searchParams.get('name') ?? "User"));
 }
 
 // Run server: deno run --allow-net server.ts
